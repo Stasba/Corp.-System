f9bb7fc80703415f120a7e66e00d77b7678db098
Stasba <111207743+Stasba@users.noreply.github.com>
2025-01-23 21:20:50 +0300

    Update and rename Lab4_Var7.py to Lab4_Var8.py

--- Lab4_Var7.py
+++
@@ -1,9 +0,0 @@
-n= int(input("натуральное n - "))
-
-current_factorial = 1
-factorial_sum = 0
-
-for i in range(1, n + 1):
-    current_factorial *=i
-    factorial_sum +=current_factorial
-print("Сумма от 1 до", n, "равна", factorial_sum)
